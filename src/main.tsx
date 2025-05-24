ed15a3961310bb65249a1f04f3c26b0f854238b1
dev <almas1610@gmail.com>
2025-05-24 17:01:52 +0500

    [deps]: added react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { 
+  createBrowserRouter,
+  RouterProvider  
+} from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: "forms",
+        element: <div><h1>Forms</h1></div>
+      }
+    ]
+  },
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
